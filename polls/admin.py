05b075df7c2b2aa9d13f20d1c655c6b50c9f9f51
Manuel <nunyes_m_a@ukr.net>
2016-03-16 23:56:55 +0200

    Fixed and updated models.py and admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import TableName,TableDocument
+from .models import People,Document
 # Register your models here.
 class AdminTable(admin.ModelAdmin):
-	edication = "edication"
+	people_id = "people_id"
 
-admin.site.register(TableName,AdminTable)	
-
+admin.site.register(People)	
+admin.site.register(Document,AdminTable)
 	
--- polls/models.py
+++ polls/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class TableName(models.Model):
-	
+class People(models.Model):
 	name = models.CharField(max_length=200)
-
 	def __unicode__(self):
 		return self.name
 
-class TableDocument(models.Model):
+	def __str__(self):
+		return self.name
 
-	people_id = models.ForeignKey(TableName)
+
+class Document(models.Model):
+
+	people_id = models.ForeignKey(People)
 	edication = models.CharField(max_length=200)
 
 	def __unicode__(self):
-		return "%s %s" % (people_id,edication)
+		return "%s %s" % (self.people_id,self.edication)
+
+	def __str__(self):
+		return "%s %s" % (self.people_id,self.edication)
+		
 
 		
